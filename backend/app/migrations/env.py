e83ce53575f126e3c11d81148b097ad0be03527f
Alexander Golovnya <algol.mail@gmail.com>
2021-04-22 01:03:56 +0700

    Add small updates.

--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -1,10 +1,14 @@
 import os
+import sys
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +22,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from db.base import Base  # noqa
+from db.base import Base
 
 target_metadata = Base.metadata
 
