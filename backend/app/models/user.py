b126d5a7f55c78f3dfd5f6dab5e08cbd4d22b697
Alexander Golovnya <algol.mail@gmail.com>
2021-05-12 23:39:34 +0700

    Start refactor User model and auth.

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,10 +1,15 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from sqlalchemy import Boolean, Column, String
 from sqlalchemy.ext.associationproxy import association_proxy
+
 from db.base_class import Base
 
 
-class User(Base, SQLAlchemyBaseUserTable):
+class User(Base):
+    email = Column(String(length=320), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(length=72), nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+    is_verified = Column(Boolean, default=False, nullable=False)
     # association proxy of "user_companies" collection
     # to "company" attribute
     companies = association_proxy('user_companies', 'company')
-
