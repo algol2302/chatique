9d223cf77de4ca302af8ff99d5d93e4927d2bd69
Alexander Golovnya <algol.mail@gmail.com>
2021-06-03 00:56:55 +0700

    Add websockets.

--- backend/app/api/api_v1/endpoints/chat.py
+++ backend/app/api/api_v1/endpoints/chat.py
@@ -49,4 +49,5 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
+        print(data)
         await websocket.send_text(f"Message text was: {data}")
